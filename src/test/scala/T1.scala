7289f76b3a93ec9cfda810dce9297fa21686eb39
Amir Shaikhha <amir.shaikhha@epfl.ch>
2013-10-25 16:58:02 +0200

    Eugene's solution

--- src/test/scala/T1.scala
+++ src/test/scala/T1.scala
@@ -4,8 +4,8 @@ object T1 {
   def main(args: Array[String]) {
     // Lifter.m[A]
     // Lifter.m[T]
-    Lifter.m[Ta]
+    // Lifter.m[Ta]
     // Lifter.m[U]
-    // Lifter.m[V]
+    Lifter.m[V]
   }
 }
